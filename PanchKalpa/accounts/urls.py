a1f1d1eaa2aa791b4ff0e2ad11f41cdefc54c4da
SRohan <shendgerohan74@gmail.com>
2025-11-24 15:34:43 +0530

    done

--- PanchKalpa/accounts/urls.py
+++ PanchKalpa/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import login_view, signup_view, logout_view
+from .views import login_view, signup_view, logout_view, therapist_signup
 
 urlpatterns = [
     path("login/", login_view, name="login"),
     path("signup/", signup_view, name="signup"),
     path("logout/", logout_view, name="logout"),
+    path("therapist-signup/", therapist_signup, name='doc_signup'),
+
 ]
