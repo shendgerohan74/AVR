3c2076092c11ae73a72610a3a442b5ccd67140fe
Aniket Gaikwad <aniketagaikwad2727@gmail.com>
2025-12-09 11:13:29 +0530

    aniket done

--- accounts/urls.py
+++ accounts/urls.py
@@ -22,16 +22,18 @@
 #     path("therapist-signup/", therapist_signup, name="therapist-signup"),
 # ]
 
+from accounts.views import otp_login
 
 from django.urls import path
-from .views import login_view, signup_view, logout_view, therapist_signup, landing_page
+from .views import otp_login, signup_view, logout_view, therapist_signup, landing_page, send_otp
 
 urlpatterns = [
     path("", landing_page, name="landing"),  # THIS NOW WORKS
 
-    path("login/", login_view, name="login"),
+    path('login/', otp_login, name='login'),
     path("signup/", signup_view, name="signup"),
     path("logout/", logout_view, name="logout"),
+    path('send-otp/', send_otp, name='send-otp'),
 
     # Therapist Signup Not Allowed
     path("therapist-signup/", therapist_signup, name="therapist-signup"),
--- chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("send/", views.send_message),
-    path("get/<int:therapist_id>/", views.get_messages),
-]
